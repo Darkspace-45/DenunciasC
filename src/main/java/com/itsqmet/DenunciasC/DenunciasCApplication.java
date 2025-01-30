126efa6ee97ad47aaf1c76cb7537193fb2906e27
Darkspace-45 <bsrp2005@gmail.com>
2025-01-30 09:58:48 -0500

    Taller Denuncias Ciudadanas

---
+++ src/main/java/com/itsqmet/DenunciasC/DenunciasCApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.DenunciasC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenunciasCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenunciasCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsqmet/DenunciasC/DenunciasCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.DenunciasC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenunciasCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
